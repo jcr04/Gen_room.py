9889fb3e62a61391e93f9e2d5740e70b69a11158
jcr04 <joaocarlosreboucas15@gmail.com>
2023-09-01 11:46:34 -0300

    add new features

--- src/Domain/Repositories/room_repository.py
+++ src/Domain/Repositories/room_repository.py
@@ -1,8 +1,14 @@
+# Importe a classe Room do módulo correto
 from Domain.Entities.room import Room
 
 class RoomRepository:
     def __init__(self):
         self.rooms = []
+
+        # Adicione algumas salas diretamente no construtor
+        self.create_room("Sala 101")
+        self.create_room("Sala 102")
+        self.create_room("Sala 103")
         
     def find_all(self):
        return self.rooms
