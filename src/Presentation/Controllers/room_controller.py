ae050af0996e39b4787a8b7a51ab3a09746592ea
jcr04 <joaocarlosreboucas15@gmail.com>
2023-09-02 21:56:11 -0300

    add news features

--- src/Presentation/Controllers/room_controller.py
+++ src/Presentation/Controllers/room_controller.py
@@ -1,80 +1,190 @@
-# src/Presentation/Controllers/room_controller.py
+# Importe as bibliotecas necessárias para o Swagger
 from flask import Blueprint, jsonify, request
 from Application.Services.room_service import RoomService
+from Domain.Entities.room import Room
+from flask_restful_swagger import swagger
 
 room_app = Blueprint('room_app', __name__)
 room_service = RoomService()
 
+# Documentação do endpoint GET /rooms
 @room_app.route('/rooms', methods=['GET'])
+@swagger.operation(
+    notes='Obtém a lista de todas as salas',
+    responseClass=[Room.__name__],  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='getRooms'
+)
 def get_rooms():
+    """
+    Obtém a lista de todas as salas.
+
+    Retorna a lista de todas as salas disponíveis.
+    """
     rooms = room_service.get_all_rooms()
     rooms_json = [room.to_json() for room in rooms]
     return jsonify(rooms_json)
 
+# Documentação do endpoint POST /rooms
 @room_app.route('/rooms', methods=['POST'])
+@swagger.operation(
+    notes='Cria uma nova sala',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='createNewRoom'
+)
 def create_new_room():
+    """
+    Cria uma nova sala.
+
+    Cria uma nova sala com base nos dados fornecidos.
+    """
     data = request.get_json()
     name = data.get('name')
-    room_type = data.get('room_type')  # Adicione esta linha para obter o tipo de sala
+    room_type = data.get('room_type')
 
-    new_room = room_service.create_room(name, room_type)  # Passe o room_type
-
+    new_room = room_service.create_room(name, room_type)
     return jsonify(new_room.to_json()), 201
 
+# Documentação do endpoint POST /rooms/{room_id}/reserve
 @room_app.route('/rooms/<string:room_id>/reserve', methods=['POST'])
+@swagger.operation(
+    notes='Reserva uma sala',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='reserveRoom'
+)
 def reserve_room(room_id):
+    """
+    Reserva uma sala.
+
+    Reserva uma sala com base no ID da sala fornecido.
+    """
     result = room_service.reserve_room(room_id)
     if result:
         room_details = room_service.get_room_details(room_id)
         return jsonify({'message': 'Room reserved successfully', 'room_details': room_details}), 200
     return jsonify({'error': 'Room not found or already occupied'}), 404
 
+# Continue documentando os outros endpoints da mesma forma
+
+# Documentação do endpoint GET /rooms/occupied
 @room_app.route('/rooms/occupied', methods=['GET'])
+@swagger.operation(
+    notes='Obtém a lista de salas ocupadas',
+    responseClass=[Room.__name__],  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='getOccupiedRooms'
+)
 def get_occupied_rooms():
+    """
+    Obtém a lista de salas ocupadas.
+
+    Retorna a lista de todas as salas que estão atualmente ocupadas.
+    """
     occupied_rooms = room_service.get_occupied_rooms()
     return jsonify(occupied_rooms)
 
+# Documentação do endpoint GET /rooms/{room_id}
 @room_app.route('/rooms/<string:room_id>', methods=['GET'])
+@swagger.operation(
+    notes='Obtém detalhes de uma sala específica',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='getRoomDetails'
+)
 def get_room_details(room_id):
+    """
+    Obtém detalhes de uma sala específica.
+
+    Retorna os detalhes de uma sala com base no ID da sala fornecido.
+    """
     room_details = room_service.get_room_details(room_id)
     if room_details:
         return jsonify(room_details), 200
     return jsonify({'error': 'Room not found'}), 404
 
+# Documentação do endpoint DELETE /rooms/{room_id}
 @room_app.route('/rooms/<string:room_id>', methods=['DELETE'])
+@swagger.operation(
+    notes='Exclui uma sala',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='deleteRoom'
+)
 def delete_room(room_id):
+    """
+    Exclui uma sala.
+
+    Exclui uma sala com base no ID da sala fornecido.
+    """
     result = room_service.delete_room(room_id)
     if result:
         return jsonify({'message': 'Room deleted successfully'}), 200
     return jsonify({'error': 'Room not found'}), 404
 
+# Documentação do endpoint GET /rooms/available
 @room_app.route('/rooms/available', methods=['GET'])
+@swagger.operation(
+    notes='Obtém a lista de salas disponíveis',
+    responseClass=[Room.__name__],  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='getAvailableRooms'
+)
 def get_available_rooms():
+    """
+    Obtém a lista de salas disponíveis.
+
+    Retorna a lista de todas as salas que estão atualmente disponíveis.
+    """
     available_rooms = room_service.get_available_rooms()
     return jsonify([room.to_json() for room in available_rooms])
 
+# Documentação do endpoint PUT /rooms/{room_id}/update-name
 @room_app.route('/rooms/<string:room_id>/update-name', methods=['PUT'])
+@swagger.operation(
+    notes='Atualiza o nome de uma sala',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='updateRoomName'
+)
 def update_room_name(room_id):
+    """
+    Atualiza o nome de uma sala.
+
+    Atualiza o nome de uma sala com base no ID da sala fornecido e no novo nome fornecido.
+    """
     data = request.get_json()
     new_name = data.get('new_name')
-
     updated_room = room_service.update_room_name(room_id, new_name)
     if updated_room:
         return jsonify({'message': 'Room name updated successfully', 'room': updated_room.to_json()}), 200
     return jsonify({'error': 'Room not found'}), 404
 
+# Documentação do endpoint GET /rooms/by-type/{room_type}
 @room_app.route('/rooms/by-type/<string:room_type>', methods=['GET'])
+@swagger.operation(
+    notes='Obtém a lista de salas por tipo',
+    responseClass=[Room.__name__],  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='getRoomsByType'
+)
 def get_rooms_by_type(room_type):
-    rooms = room_service.get_rooms_by_type(room_type)
-    rooms_json = [room.to_json() for room in rooms]
-    return jsonify(rooms_json)
+    """
+    Obtém a lista de salas por tipo.
 
+    Retorna a lista de todas as salas que correspondem ao tipo fornecido.
+    """
+    rooms = room_service.get_rooms_by_type(room_type)
+    return jsonify([room.to_json() for room in rooms])
+
+# Documentação do endpoint POST /rooms/{room_id}/reserve-by-period
 @room_app.route('/rooms/<string:room_id>/reserve-by-period', methods=['POST'])
+@swagger.operation(
+    notes='Reserva uma sala por período',
+    responseClass=Room.__name__,  # Use a classe apropriada aqui (Room representa o exemplo)
+    nickname='reserveRoomByPeriod'
+)
 def reserve_room_by_period(room_id):
+    """
+    Reserva uma sala por período.
+
+    Reserva uma sala com base no ID da sala fornecido e no período especificado.
+    """
     data = request.get_json()
     start_time = data.get('start_time')
     end_time = data.get('end_time')
-
     result = room_service.reserve_room_by_period(room_id, start_time, end_time)
 
     if 'error' in result:
--- src/main.py
+++ src/main.py
@@ -1,9 +1,33 @@
 # main.py
 from flask import Flask
 from Presentation.Controllers.room_controller import room_app
+from flask import Flask
+from flask_restful import Api
+from flask_restful_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+api = Api(app)
 
+# Defina as informações gerais da API
+app.config['SWAGGER_INFO'] = {
+    'title': 'Sua API de Salas',
+    'version': '1.0',
+    'description': 'Documentação interativa da API de Salas',
+}
+
+# Configure o Swagger UI
+SWAGGER_URL = '/api/swagger'  # URL da página do Swagger
+API_URL = '/static/swagger.json'  # URL da especificação Swagger em JSON
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sua API de Salas"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 app.register_blueprint(room_app, url_prefix='/api')
 
 if __name__ == '__main__':
