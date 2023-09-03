32322c2867b80adb42aafeb72fa6e85df3baef93
jcr04 <joaocarlosreboucas15@gmail.com>
2023-09-02 23:12:50 -0300

    add

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 # main.py
 from flask import Flask
-from Presentation.Controllers.room_controller import room_app
-from flask import Flask
 from flask_restful import Api
 from flask_swagger_ui import get_swaggerui_blueprint
-from flask_restful_swagger import swagger
+from Presentation.Controllers.room_controller import room_app
+from flask_restplus import Api, Resource
+
 app = Flask(__name__)
 api = Api(app)
+
+# Mapeie a URL '/static' para a pasta '/static' no seu sistema de arquivos
+app.static_url_path = '/static'
+app.static_folder = 'static'
 
 # Defina as informações gerais da API
 app.config['SWAGGER_INFO'] = {
@@ -17,7 +21,7 @@ app.config['SWAGGER_INFO'] = {
 
 # Configure o Swagger UI
 SWAGGER_URL = '/api/swagger'  # URL da página do Swagger
-API_URL = '/static/swagger.json'  # URL da especificação Swagger em JSON
+API_URL = '/static/swagger.json'  # Verifique se esta URL está correta
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
@@ -30,4 +34,4 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 app.register_blueprint(room_app, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
