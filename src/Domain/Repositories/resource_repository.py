04099e658f035bd35529744f399c980a594909b3
jcr04 <joaocarlosreboucas15@gmail.com>
2023-09-21 18:40:43 -0300

    add news features

--- src/Domain/Repositories/resource_repository.py
+++ src/Domain/Repositories/resource_repository.py
@@ -1,41 +1,45 @@
+from typing import List, Optional, Union
 from Domain.Entities.resource import Resource
 
 class ResourceRepository:
     def __init__(self):
-        self.resources = []
+        self.resources: List[Resource] = []
         
-        self.create_resource('Projetor', 'Projetor de alta definição' )
-        self.create_resource('Notebook', 'Notebook para apresentações' )
-        self.create_resource('Quadro branco', 'Quadro branco interativo' )
+        # Criando alguns recursos iniciais
+        self.create_resource('Projetor', 'Projetor de alta definição')
+        self.create_resource('Notebook', 'Notebook para apresentações')
+        self.create_resource('Quadro branco', 'Quadro branco interativo')
         
-    def find_all(self):
+    def find_all(self) -> List[Resource]:
+        """Retorna todos os recursos disponíveis."""
         return self.resources
 
-    def find_by_id(self, id):
+    def find_by_id(self, resource_id: str) -> Optional[Resource]:
+        """Retorna o recurso correspondente ao ID fornecido."""
         for resource in self.resources:
-            if resource.id == id:
+            if resource.id == resource_id:
                 return resource
         return None
 
-    def create_resource(self, name, description):
+    def create_resource(self, name: str, description: str) -> Resource:
+        """Cria um novo recurso e o adiciona à lista de recursos."""
         new_resource = Resource(str(len(self.resources) + 1), name, description)
         self.resources.append(new_resource)
         return new_resource
 
-    def delete_resource(self, resource):
+    def delete_resource(self, resource: Resource) -> bool:
+        """Remove o recurso da lista de recursos."""
         if resource in self.resources:
             self.resources.remove(resource)
             return True
         return False
 
-    def reserve_resource(self, resource_id, start_time, end_time):
+    def reserve_resource(self, resource_id: str, start_time: str, end_time: str) -> bool:
+        """Reserva um recurso para um período de tempo especificado."""
         resource = self.find_by_id(resource_id)
         if resource:
             for reservation in resource.reservations:
-                if (
-                    start_time < reservation['end_time']
-                    and end_time > reservation['start_time']
-                ):
+                if start_time < reservation['end_time'] and end_time > reservation['start_time']:
                     return False  # O recurso já está reservado neste período
 
             new_reservation = {
@@ -46,7 +50,8 @@ class ResourceRepository:
             return True
         return False
 
-    def get_resource_reservations(self, resource_id):
+    def get_resource_reservations(self, resource_id: str) -> List[dict]:
+        """Retorna todas as reservas para o recurso com o ID fornecido."""
         resource = self.find_by_id(resource_id)
         if resource:
             return resource.reservations
--- src/Presentation/Controllers/resource_controller.py
+++ src/Presentation/Controllers/resource_controller.py
@@ -1,15 +1,10 @@
-# No arquivo src\Presentation\Controllers\resource_controller.py
-from http.client import NOT_FOUND
-from flask import Blueprint, app, jsonify, request
+from flask import Blueprint, jsonify, request
 from flask_restful_swagger import swagger
 from Application.Services.resource_service import ResourceService
 from Domain.Entities.resource import Resource
-from Domain.Repositories.resource_repository import ResourceRepository
 
 resource_app = Blueprint('resource_app', __name__)
 resource_service = ResourceService()
-
-resource_repository = ResourceRepository()
 
 @resource_app.route('/api/resources', methods=['GET'])
 @swagger.operation(
@@ -18,17 +13,8 @@ resource_repository = ResourceRepository()
     nickname='listResources'
 )
 def list_resources():
-    resource_repository = ResourceRepository()
-    resources = resource_repository.find_all()
-
-    resource_list = [
-        {
-            **resource.to_json(),
-            'is_reserved': True if resource.is_reserved else False
-        }
-        for resource in resources
-    ]
-
+    resources = resource_service.get_all_resources()
+    resource_list = [{**resource.to_json(), 'is_reserved': resource.is_reserved} for resource in resources]
     return jsonify(resource_list), 200
 
 @resource_app.route('/api/resources', methods=['POST'])
@@ -41,8 +27,10 @@ def create_resource():
     data = request.get_json()
     name = data.get('name')
     description = data.get('description')
+    if not name or not description:
+        return jsonify({'error': 'Nome e descrição são obrigatórios'}), 400
     created_resource = resource_service.create_resource(name, description)
-    return jsonify(created_resource.to_json()), 201  # Retorna o recurso criado
+    return jsonify(created_resource.to_json()), 201
 
 @resource_app.route('/api/resources/<string:resource_id>', methods=['DELETE'])
 @swagger.operation(
@@ -50,12 +38,11 @@ def create_resource():
     responseClass=bool.__name__,
     nickname='deleteResource'
 )
-def delete_resource(resource_id):
+def delete_resource(resource_id: str):
     success = resource_service.delete_resource(resource_id)
     if success:
-        return 'Recurso excluído com sucesso', 204
-    else:
-        return 'Recurso não encontrado', 404
+        return jsonify({'message': 'Recurso excluído com sucesso'}), 204
+    return jsonify({'error': 'Recurso não encontrado'}), 404
 
 @resource_app.route('/api/resources/<string:resource_id>/reservations', methods=['GET'])
 @swagger.operation(
@@ -63,7 +50,7 @@ def delete_resource(resource_id):
     responseClass=list.__name__,
     nickname='getReservations'
 )
-def get_resource_reservations(resource_id):
+def get_resource_reservations(resource_id: str):
     reservations = resource_service.get_resource_reservations(resource_id)
     return jsonify(reservations), 200
 
@@ -73,15 +60,13 @@ def get_resource_reservations(resource_id):
     responseClass=Resource.__name__,
     nickname='reserveResource'
 )
-def reserve_resource(resource_id):
-    resource_repository = ResourceRepository()  # Crie uma instância do repositório
-    resource = resource_repository.find_by_id(resource_id)
+def reserve_resource(resource_id: str):
+    resource = resource_service.find_by_id(resource_id)
     if resource is None:
-        return jsonify({'message': 'Recurso não encontrado'}), 404
+        return jsonify({'error': 'Recurso não encontrado'}), 404
 
     resource.is_reserved = True
     return jsonify({
         'message': 'Recurso reservado com sucesso',
-        'resource_details': resource.to_json()  # Retorne os detalhes do recurso atualizados em JSON
+        'resource_details': resource.to_json()
     }), 200
-
